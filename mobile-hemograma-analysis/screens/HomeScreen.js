1de2dc330d383172f242aafa158f51c5235c17ca
Stephany Milhomem <stephanymilhomem25@gmail.com>
2025-09-08 00:06:07 -0300

    🎉 Start mobile project with the right settings

---
+++ mobile-hemograma-analysis/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo à Tela Principal!</Text>
+      <Text>Aqui ficará a dashboard com o gráfico.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+});
