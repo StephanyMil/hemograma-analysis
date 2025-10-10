ac0c552324b4ab5515011a1641784190d9aa735d
Aline <alinelmcoelho@gmail.com>
2025-10-10 19:32:38 -0300

    Adiciona endpoints de leitura de hemogramas para o frontend

--- backend-hemograma-analysis/src/main/java/com/inf/ubiquitous/computing/backend_hemograma_analysis/user/controller/SyntheticHemogramController.java
+++ backend-hemograma-analysis/src/main/java/com/inf/ubiquitous/computing/backend_hemograma_analysis/user/controller/SyntheticHemogramController.java
@@ -1,7 +1,5 @@
 package com.inf.ubiquitous.computing.backend_hemograma_analysis.user.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +16,7 @@ public class SyntheticHemogramController {
     private SyntheticHemogramGeneratorService hemogramGeneratorService;
 
     @GetMapping("/generate")
-    public List<String> generate(@RequestParam(defaultValue = "5") int qtde) {
+    public String generate(@RequestParam(defaultValue = "5") int qtde) {
         return hemogramGeneratorService.gerarHemogramasSinteticos(qtde);
     }
 }
